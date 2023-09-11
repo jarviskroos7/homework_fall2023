b59d648fd4f620bafa9b482763a911c1ad136f9a
jarvis <jarviskroos7@berkeley.edu>
2023-09-11 00:57:47 -0700

    submitted hw1

--- hw1/cs285/scripts/run_hw1.py
+++ hw1/cs285/scripts/run_hw1.py
@@ -20,6 +20,8 @@ from cs285.infrastructure.replay_buffer import ReplayBuffer
 from cs285.policies.MLP_policy import MLPPolicySL
 from cs285.policies.loaded_gaussian_policy import LoadedGaussianPolicy
 
+import warnings
+warnings.filterwarnings('ignore')
 
 # how many rollouts to save as videos to tensorboard
 MAX_NVIDEO = 2
@@ -132,7 +134,9 @@ def run_training_loop(params):
             # TODO: collect `params['batch_size']` transitions
             # HINT: use utils.sample_trajectories
             # TODO: implement missing parts of utils.sample_trajectory
-            paths, envsteps_this_batch = TODO
+            paths, envsteps_this_batch = utils.sample_trajectories(
+                env, actor, params['batch_size'], params['ep_len']
+            )
 
             # relabel the collected obs with actions from a provided expert policy
             if params['do_dagger']:
@@ -141,7 +145,9 @@ def run_training_loop(params):
                 # TODO: relabel collected obsevations (from our policy) with labels from expert policy
                 # HINT: query the policy (using the get_action function) with paths[i]["observation"]
                 # and replace paths[i]["action"] with these expert labels
-                paths = TODO
+                for path in paths:
+                    path['action'] = expert_policy.get_action(path['observation'])
+                # paths = TODO
 
         total_envsteps += envsteps_this_batch
         # add collected data to replay buffer
@@ -157,7 +163,8 @@ def run_training_loop(params):
           # HINT2: use np.random.permutation to sample random indices
           # HINT3: return corresponding data points from each array (i.e., not different indices from each array)
           # for imitation learning, we only need observations and actions.  
-          ob_batch, ac_batch = TODO
+          idx = np.random.permutation(replay_buffer.obs.shape[0])[:params['train_batch_size']]
+          ob_batch, ac_batch = replay_buffer.obs[idx], replay_buffer.acs[idx]
 
           # use the sampled data to train an agent
           train_log = actor.update(ob_batch, ac_batch)
